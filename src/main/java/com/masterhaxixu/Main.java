7f32a9bed1749f9d597a4c4506ae2d2e98d356a0
MasterHaxixu <80980812+MasterHaxixu@users.noreply.github.com>
2023-09-06 22:44:48 +0200

    Fixed error handling

--- src/main/java/com/masterhaxixu/Main.java
+++ src/main/java/com/masterhaxixu/Main.java
@@ -6,8 +6,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.md_5.bungee.api.ChatColor;
+
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,27 +25,33 @@ public class Main extends JavaPlugin {
         Map<EntityType, Integer> entityCounts = new HashMap<>();
         if (cmd.getName().equalsIgnoreCase("entitycounter")) {
             if (args.length > 0) {
-            World world = null;
-            if (args[0].equalsIgnoreCase("world_the_end") || args[0].equalsIgnoreCase("world_nether") || args[0].equalsIgnoreCase("world")) {
-                world = getServer().getWorld(args[0].toLowerCase());
-            }
-            for (World loadedWorld : world != null ? Collections.singletonList(world) : getServer().getWorlds()) {
-                loadedWorld.getEntities().forEach(entity -> {
-                    EntityType entityType = entity.getType();
-                    entityCounts.put(entityType, entityCounts.getOrDefault(entityType, 0) + 1);
-                });
-            }
-        } else {
+                World world = getServer().getWorlds().stream()
+                        .filter(w -> args[0].equalsIgnoreCase(w.getName()))
+                        .findFirst()
+                        .orElse(null);
+                if (world != null) {
+
+                    world.getEntities().forEach(entity -> {
+                        EntityType entityType = entity.getType();
+                        entityCounts.put(entityType, entityCounts.getOrDefault(entityType, 0) + 1);
+                    });
+
+                } else
+                    sender.sendMessage("No world with the name " + ChatColor.RED + args[0] + " was found!");
+
+            } else {
                 getServer().getWorlds().forEach(world -> {
-                world.getEntities().forEach(entity -> {
-                    EntityType entityType = entity.getType();
-                    entityCounts.put(entityType, entityCounts.getOrDefault(entityType, 0) + 1);
+                    world.getEntities().forEach(entity -> {
+                        EntityType entityType = entity.getType();
+                        entityCounts.put(entityType, entityCounts.getOrDefault(entityType, 0) + 1);
+                    });
                 });
-            });
             }
             String msg = "Entity Counts in $WORLD:";
-            if(args.length > 0) sender.sendMessage(msg.replace("$WORLD", args[0]));
-            else sender.sendMessage(msg.replace("$WORLD", "All Worlds"));
+            if (args.length > 0)
+                sender.sendMessage(msg.replace("$WORLD", args[0]));
+            else
+                sender.sendMessage(msg.replace("$WORLD", "All Worlds"));
             for (Map.Entry<EntityType, Integer> entry : entityCounts.entrySet()) {
                 sender.sendMessage(entry.getKey().name() + ": " + entry.getValue());
             }
@@ -52,6 +59,7 @@ public class Main extends JavaPlugin {
         }
         return false;
     }
+
     public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("entitycounter")) {
             List<String> completions = new ArrayList<>();
